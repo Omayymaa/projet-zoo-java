19ffee42b3cb9fc9b7071cd4fc50eccfe34b5e91
GouiderOmaima <omaimagouider6@gmail.com>
2025-09-18 10:01:46 +0100

    commit1

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
 public class ZooManagement {
-      static int nbrCages;
-      static String zooName;
 
-      public static void main(String[] args) {
-          Scanner sc = new Scanner(System.in);
-          System.out.print("entrez le nom du zoo : ");
-          zooName = sc.nextLine();
-          while (zooName.isEmpty()){
-              System.out.print("le nom ne doit pas etre vide. réessayez : ");
-              zooName = sc.nextLine();
-          }
-          System.out.print("entrez le nombre de cages : ");
-          nbrCages = sc.nextInt();
-          while(nbrCages <= 0) {
-              System.out.print("le nombre doit etre positif. Réessayez : ");
-              nbrCages = sc.nextInt();
-          }
-          sc.close();
-          System.out.println(zooName + " comporte " + nbrCages + " cages.");
-      }
+    public static void main(String[] args) {
+        int nbrCages;
+        String zooName;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entrez le nom du zoo : ");
+        zooName = sc.nextLine();
+        while (zooName.isEmpty()) {
+            System.out.print("Le nom ne doit pas être vide. Réessayez : ");
+            zooName = sc.nextLine();
+        }
+
+        System.out.print("Entrez le nombre de cages : ");
+        nbrCages = sc.nextInt();
+        while (nbrCages <= 0) {
+            System.out.print("Le nombre doit être positif. Réessayez : ");
+            nbrCages = sc.nextInt();
+        }
+
+        sc.close();
+        System.out.println(zooName + " comporte " + nbrCages + " cages.");
+    }
 }
